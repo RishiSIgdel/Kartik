5b4f7e0c00afebfdbf9d9e0cce44f43b2a39fc02
RishiSIgdel <rishisigdel16@gmail.com>
2022-12-05 12:55:33 +0545

    New added

---
+++ homework.js
@@ -0,0 +1,25 @@
+// let person = {
+//     name: "John",
+//     height: "5",
+//     age: 20,
+//     // age: 30,
+//     // TODO:
+//     // Age: 30,
+//     1: "one",
+//     // 2: "two"
+// }
+
+// console.log("result=", person["age"]);
+// // person.name = "changed John"
+// // console.log(person.name)
+// // console.log(person[1])
+
+let a = 1;
+let b = 2;
+let result = a + b;
+
+console.log('the sum of ' + a + ' and ' + b + ' is ' + result);
+a = 5;
+b = 6;
+result = a + b;
+console.log('the sum of ' + a + ' and ' + b + ' is ' + result);
